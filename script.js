65bae64d1d134478ba9f464215a06816f075470e
Innocent Chosen Chukwuebuka <113795691+innochosen@users.noreply.github.com>
2023-08-06 15:19:00 +0100

    main

---
+++ script.js
@@ -0,0 +1,12 @@
+
+        //var myName = "Chosen"
+        // var yourName = prompt("What Is Your Name?");
+        // alert("My Name is " + myName + ", welcome to my course " + yourName + "!")
+    
+        // var result = confirm ("Do you really want to leave this page?");
+        // if (result == true) {
+        //     alert(Thanks for visiting);
+        // }
+        // else{
+        //     alert("Thanks for staying with us");
+        // }
